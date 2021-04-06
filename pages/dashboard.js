b1dcaacf76014581e1fe85b26a25f899dd78992c
Hauke Bartsch <HaukeBartsch@gmail.com>
2021-04-06 13:50:38 +0200

    finish the export as a series of CSV files feature

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -10,6 +10,12 @@ const {
 } = require('../resourcesConfig');
 
 export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.convertToCSV = this.convertToCSV.bind(this);
+  }
+
+
   static async getInitialProps({ req, res }) {
     const user = await authenticate(req, res);
     if (typeof req === 'undefined') {
@@ -41,15 +47,79 @@ export default class Dashboard extends React.Component {
       window.localStorage.remove('email');
       window.localStorage.remove('oneup_access_token');
     }
+    this.documentLink = document;
   }
 
   shareWithABCD() {
 
   }
 
-  convertToCSV() {
-    var res = this.props.dashboard.resources;
-    console.log(JSON.stringify(res));  // are we on the server or are we on the client?
+  flattenObject(ob) {
+    var toReturn = {};
+
+    for (var i in ob) {
+        if (!ob.hasOwnProperty(i)) continue;
+
+        if ((typeof ob[i]) == 'object' && ob[i] !== null) {
+            var flatObject = this.flattenObject(ob[i]);
+            for (var x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = ob[i];
+        }
+    }
+    return toReturn;
+  }
+
+  downloadAll(files){
+    if (files.length == 0) 
+       return;
+    var file = files.pop();
+    var a = this.documentLink.createElement("A");
+    a.href = file[1];
+    a.download = file[0];
+    this.documentLink.body.appendChild(a);
+    a.click();
+    this.documentLink.body.removeChild(a);
+    setTimeout( function() { this.downloadAll(files); }.bind(this), 10);
+  }
+
+  convertToCSV(e) {
+    console.log(JSON.stringify(this.props));
+    if (typeof this.props.dashboard !== 'undefined') {
+      var res = this.props.dashboard.resources;
+      var resNames = Object.keys(this.props.dashboard.resources);
+      // for each resource (Patient, ...) we can create one CSV file (array of arrays)
+      var allData = [];
+      for (var i = 0; i < resNames.length; i++) {
+        var o = this.props.dashboard.resources[resNames[i]];
+        if (this.props.dashboard.resources[resNames[i]].entry.length == 0) {
+          continue;
+        }
+        // now flatten the structure
+        var out = this.flattenObject(o);
+        // we can convert this flat object into an array of arrays (CSV)
+        var arrayRepresentation = new Array( Object.keys(out) );
+        arrayRepresentation.push(Object.values(out));
+        console.log(JSON.stringify(arrayRepresentation));
+        var txtAsCsv = arrayRepresentation.map(function(a) { return a.map(function(a) { return JSON.stringify(a); }).join(","); }).join("\r\n");
+        var file_path = "data:application/csv;charset=utf-8," + encodeURI(txtAsCsv);
+        allData.push([ resNames[i] + ".csv", file_path]);
+        /*
+        var a = this.documentLink.createElement("A");
+        a.href = file_path; // "/exported_" + resNames[i] + "/" + resNames[i] + ".csv";
+        a.download = resNames[i] + ".csv";
+        this.documentLink.body.appendChild(a);
+        a.click();
+        this.documentLink.body.removeChild(a);
+        */
+      }
+      this.downloadAll(allData);
+      console.log(JSON.stringify(res));  // we are in react.. but it works if we place a breakpoint in the browser
+    }
   }
 
   render() {
@@ -58,8 +128,10 @@ export default class Dashboard extends React.Component {
         <Header user={this.props.user} />
         <div className="container">
           <br />
-          <h1>Your medical dashboard </h1>
-          <p>Review your data and agree below to share with ABCD or just download as a spreadsheet.</p>
+          <h1>Your medical information </h1>
+          <p>Review your data and agree below to share with ABCD. You may review the content of the shared data as a spreadsheet (Review data as CSV).</p>
+          <br />
+          <button class="btn btn-primary" id="ExportAsCSV" onClick={this.convertToCSV} title="Will download individual spreadsheets for all resources. Check with their file-size to find out if sufficient data was collected.">Review data as CSV</button>
           <br />
           <div>
             {typeof this.props.dashboard.resources.Patient !== 'undefined' &&
@@ -78,7 +150,7 @@ export default class Dashboard extends React.Component {
                 </div>
               )}
           </div>
-          <div style={{ textAlign: 'left' }}>
+          <div style={{ textAlign: 'left', height: '600px', "overflow-y": 'scroll', "background": "#EEE", "margin-top": "10px", "margin-bottom": "10px" }}>
             {resourcesListToDisplayInOrder.map(
               function (resourceType) {
                 return (
@@ -111,8 +183,7 @@ export default class Dashboard extends React.Component {
             )}
           </div>
           <div>
-          <button class="btn btn-primary" id="ShareWithABCD" onClick={this.shareWithABCD}>Share this data with ABCD</button>
-          <button class="btn btn-primary" id="ExportAsCSV" onClick={this.convertToCSV}>Download data as CSV</button>
+          <button class="btn btn-primary" id="ShareWithABCD" onClick={this.shareWithABCD} title="Currently this function is not available as it needs a proper integration into the ABCD study data infrastructure." disabled>Share this data with ABCD</button>&nbsp;
           <div class="spacer"></div>
           <div class="spacer"></div>
           </div>
